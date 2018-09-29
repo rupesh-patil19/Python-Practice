a730de20d007136f4da02febca84928c68bfed61
rupesh-patil19 <40706746+rupesh-patil19@users.noreply.github.com>
2018-09-28 19:36:34 -0700

    Update bs.py

--- bs.py
+++ bs.py
@@ -1,29 +1,21 @@
 #!usr/bin/python3
 
 def main():
-    A = [1,2,3,4,5,6,7]
-    binary(A,7)
+    Array1 = [9,22,39.3,47,59,65,73]
+    binary(Array1,47)
     
-def binary(array,element):
-        
+def binary(array,element):    
         first = 0
         last = len(array) 
-        
         while (first < last):
-            mp = round((first + last)/2)
-            
-            if element == array[mp]:
-                print("The element is at index",mp)
+            midpoint = round((first + last)/2) #round function to round the division off to nearest integer
+            if element == array[midpoint]:
+                print("The element is at index",midpoint)
                 break
-            if element < array[mp]:
-                last = mp - 1
-                
-            elif element > array[mp]:
-                first = mp + 1
-            
-        if element != array[mp]: 
-            print("The element does not exist in the list")
-           
-        
-        
+            if element < array[midpoint]:
+                last = midpoint - 1
+            elif element > array[midpoint]:
+                first = midpoint + 1
+        if element != array[midpoint]: 
+            print("The element does not exist in the array")
 if __name__ == "__main__":main()
